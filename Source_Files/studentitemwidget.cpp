08c1f7eb7b78200cd957a71121b9fd23ef97443b
Pavel <68698528+Dagor011@users.noreply.github.com>
2025-07-02 16:42:43 +0300

    Update studentitemwidget.cpp

--- Source_Files/studentitemwidget.cpp
+++ Source_Files/studentitemwidget.cpp
@@ -4,14 +4,14 @@ StudentItemWidget::StudentItemWidget(QString studentName, QWidget *parent)
     : QWidget(parent)
 {
     
-    //Виджет, отображающий имя студента
+   
     m_label = new QLabel(studentName, this);
     m_checkbox = new QCheckBox(this);
-    // Увелечение шрифта под студентов
+  
     QFont font;
     font.setPointSize(16);
     m_label->setFont(font);
-    //Макет:Имя студента слева.Растягивающийся пробел посередине.чекбокс справа.
+    
     QHBoxLayout *layout = new QHBoxLayout(this);
     layout->addWidget(m_label);
     layout->addStretch();
@@ -19,7 +19,7 @@ StudentItemWidget::StudentItemWidget(QString studentName, QWidget *parent)
    
 
     setLayout(layout);
-    //сигнал stateChanged от чекбокса к лямбде-функции, которая генерирует собственный сигнал stateChanged(bool) при изменении состояния чекбокса.
+   
     connect(m_checkbox, &QCheckBox::checkStateChanged, this, &StudentItemWidget::onCheckboxStateChanged);
     
 
